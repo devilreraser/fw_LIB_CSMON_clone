4d2ba72ac867a731faff9bd3c690b2b85aaf4c2e
dimitar.lilov <dimitar.lilov@convesys.com>
2020-05-20 23:02:29 +0300

    Empty Message

--- ConvesysToGithubAll.bat
+++ ConvesysToGithubAll.bat
@@ -75,7 +75,8 @@ echo Process Project:
 git.exe fetch --all -v --progress
 git checkout -B master --track convesys/master
 git checkout remotes/github/master
-git merge --no-commit --no-ff --allow-unrelated-histories master < ConvesysToGithub.in
+git pull github master
+git merge --no-commit --allow-unrelated-histories master < ConvesysToGithub.in
 git reset HEAD .gitattributes
 git checkout remotes/github/master -- .gitattributes
 git reset HEAD .gitmodules
