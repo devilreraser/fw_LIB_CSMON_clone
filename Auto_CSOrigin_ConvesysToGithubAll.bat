e2b1a29b6d57bd0405fb97cb98ceb2695f806169
Dimitar Lilov <dimitar.lilov@convesys.com>
2020-07-08 23:46:38 +0300

    Auto Convesys To github LFS fixed

--- Auto_CSOrigin_ConvesysToGithubAll.bat
+++ Auto_CSOrigin_ConvesysToGithubAll.bat
@@ -21,7 +21,7 @@ git pull --progress -v --no-rebase "github"
 git merge --allow-unrelated-histories -Xrenormalize remotes/convesys/master < ..\..\ConvesysToGithub.in
 git commit -a -m "%_CommitString%"
 git push github master
-git.exe checkout -f -B master origin/master --
+git.exe checkout -f -B master remotes/origin/master --
 git pull --progress -v --no-rebase "origin"
 cd ..\..
 
@@ -36,7 +36,7 @@ git pull --progress -v --no-rebase "github"
 git merge --allow-unrelated-histories -Xrenormalize remotes/convesys/master < ..\..\ConvesysToGithub.in
 git commit -a -m "%_CommitString%"
 git push github master
-git.exe checkout -f -B master origin/master --
+git.exe checkout -f -B master remotes/origin/master --
 git pull --progress -v --no-rebase "origin"
 cd ..\..
 
@@ -51,7 +51,7 @@ git pull --progress -v --no-rebase "github"
 git merge --allow-unrelated-histories -Xrenormalize remotes/convesys/master < ..\..\ConvesysToGithub.in
 git commit -a -m "%_CommitString%"
 git push github master
-git.exe checkout -f -B master origin/master --
+git.exe checkout -f -B master remotes/origin/master --
 git pull --progress -v --no-rebase "origin"
 cd ..\..
 
@@ -66,7 +66,7 @@ git pull --progress -v --no-rebase "github"
 git merge --allow-unrelated-histories -Xrenormalize remotes/convesys/master < ..\..\ConvesysToGithub.in
 git commit -a -m "%_CommitString%"
 git push github master
-git.exe checkout -f -B master origin/master --
+git.exe checkout -f -B master remotes/origin/master --
 git pull --progress -v --no-rebase "origin"
 cd ..\..
 
@@ -81,18 +81,24 @@ git pull --progress -v --no-rebase "github"
 git merge --allow-unrelated-histories -Xrenormalize remotes/convesys/master < ..\..\ConvesysToGithub.in
 git commit -a -m "%_CommitString%"
 git push github master
-git.exe checkout -f -B master origin/master --
+git.exe checkout -f -B master remotes/origin/master --
 git pull --progress -v --no-rebase "origin"
 cd ..\..
 
 echo.
 echo Process Project:
+git lfs uninstall
 git.exe fetch --all -v --progress
 git.exe checkout -f -B master remotes/convesys/master --
 git pull --progress -v --no-rebase "convesys"
+rem pause
 git.exe checkout -f -B master remotes/github/master --
+git lfs install
 git pull --progress -v --no-rebase "github"
+git lfs pull
+rem pause
 git merge --no-commit --allow-unrelated-histories -Xrenormalize remotes/convesys/master < ConvesysToGithub.in
+rem pause
 IF %ERRORLEVEL% == 0 goto CommitProject
 IF %ERRORLEVEL% NEQ 0 Echo Error = %ERRORLEVEL%
 echo Auto Resolve Merge Conflicts In PROJECT With Convesys Master:
@@ -105,13 +111,18 @@ git status
 pause >nul
 @echo off
 :CommitProject
+pause
 git reset HEAD .gitattributes
 git checkout remotes/github/master -- .gitattributes
 git reset HEAD .gitmodules
 git checkout remotes/github/master -- .gitmodules
+rem pause
 git commit -a -m "%_CommitString%"
+rem pause
 git push github master
-git.exe checkout -f -B master origin/master --
+rem pause
+git lfs uninstall
+git.exe checkout -f -B master remotes/origin/master --
 git pull --progress -v --no-rebase "origin"
 
 echo Completed!
--- Auto_CSOrigin_ConvesysToGithubNoSubmodules.bat
+++ Auto_CSOrigin_ConvesysToGithubNoSubmodules.bat
@@ -10,24 +10,42 @@ echo Commit Message: "%_CommitString%"
 
 echo.
 echo Process Project:
+git lfs uninstall
 git.exe fetch --all -v --progress
 git.exe checkout -f -B master remotes/convesys/master --
 git pull --progress -v --no-rebase "convesys"
+rem pause
 git.exe checkout -f -B master remotes/github/master --
+git lfs install
 git pull --progress -v --no-rebase "github"
+git lfs pull
+rem pause
 git merge --no-commit --allow-unrelated-histories -Xrenormalize remotes/convesys/master < ConvesysToGithub.in
+rem pause
 IF %ERRORLEVEL% == 0 goto CommitProject
 IF %ERRORLEVEL% NEQ 0 Echo Error = %ERRORLEVEL%
-echo Please Resolve Merge Conflicts In PROJECT And Press Enter To Continue
+echo Auto Resolve Merge Conflicts In PROJECT With Convesys Master:
 pause >nul
+FOR /F %%F IN ('git diff --name-only --diff-filter=U') DO echo "%%F"
+@echo on
+pause >nul
+FOR /F %%F IN ('git diff --name-only --diff-filter=U') DO git checkout convesys/master -- "%%F"
+git status
+pause >nul
+@echo off
 :CommitProject
+pause
 git reset HEAD .gitattributes
 git checkout remotes/github/master -- .gitattributes
 git reset HEAD .gitmodules
 git checkout remotes/github/master -- .gitmodules
+rem pause
 git commit -a -m "%_CommitString%"
+rem pause
 git push github master
-git.exe checkout -f -B master origin/master --
+rem pause
+git lfs uninstall
+git.exe checkout -f -B master remotes/origin/master --
 git pull --progress -v --no-rebase "origin"
 
 echo Completed!
