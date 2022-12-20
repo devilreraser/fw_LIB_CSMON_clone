3a1abc297835669d85c3bfcc66f56b65a2de1299
Dimitar Lilov <devilerser@gmail.com>
2022-12-20 15:05:07 +0200

    CreateBranchMaster.bat Added

---
+++ CreateBranchMaster.bat
@@ -0,0 +1,39 @@
+@echo off
+set BranchString=master
+set RemoteString=origin
+set RemotePath=https://github.com/devilreraser/
+set ProjectName=fw_LIB_CSMON.git
+set Module1Name=fw_LIB_HexMonitor.git
+set Module1Path=02_Libraries/HexMonitor
+set Module2Name=fw_LIB_MathAndControl.git
+set Module2Path=02_Libraries/MathAndControl
+set Module3Name=uw_LIB_ModbusAddress.git
+set Module3Path=02_Libraries/ModbusAddress
+set Module4Name=fw_LIB_Peripheral.git
+set Module4Path=02_Libraries/Peripheral
+set Module5Name=fw_LIB_TestControl.git
+set Module5Path=02_Libraries/TestControl
+set RetRootPath=..\..
+
+git.exe checkout -f -B %BranchString% remotes/%RemoteString%/%BranchString% --
+echo. Checkout '%RemoteString%/%BranchString%' path '%RemotePath%%ProjectName%'
+cd %Module1Path%
+git.exe checkout -f -B %BranchString% remotes/%RemoteString%/%BranchString% --
+echo. Checkout '%RemoteString%/%BranchString%' path '%RemotePath%%Module1Name%'
+cd %RetRootPath%
+cd %Module2Path%
+git.exe checkout -f -B %BranchString% remotes/%RemoteString%/%BranchString% --
+echo. Checkout '%RemoteString%/%BranchString%' path '%RemotePath%%Module1Name%'
+cd %RetRootPath%
+cd %Module3Path%
+git.exe checkout -f -B %BranchString% remotes/%RemoteString%/%BranchString% --
+echo. Checkout '%RemoteString%/%BranchString%' path '%RemotePath%%Module1Name%'
+cd %RetRootPath%
+cd %Module4Path%
+git.exe checkout -f -B %BranchString% remotes/%RemoteString%/%BranchString% --
+echo. Checkout '%RemoteString%/%BranchString%' path '%RemotePath%%Module1Name%'
+cd %RetRootPath%
+git.exe checkout -f -B %BranchString% remotes/%RemoteString%/%BranchString% --
+echo. Checkout '%RemoteString%/%BranchString%' path '%RemotePath%%Module1Name%'
+cd %RetRootPath%
+pause
