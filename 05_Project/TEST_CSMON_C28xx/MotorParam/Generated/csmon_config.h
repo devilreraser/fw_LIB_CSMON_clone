cc98936f45deecd7c41beae1f26252067b36c283
Dimitar Lilov <devilerser@gmail.com>
2023-01-23 18:46:43 +0200

    Generation relative path fixed

--- 05_Project/TEST_CSMON_C28xx/MotorParam/Generated/csmon_config.h
+++ 05_Project/TEST_CSMON_C28xx/MotorParam/Generated/csmon_config.h
@@ -4,8 +4,8 @@
  *
  * Created on 2019 08 14 12:51
  *
- * Generated on 01/23/2023 04:56:03 UTC
- * Generator version: 1.05
+ * Generated on 01/23/2023 16:45:22 UTC
+ * Generator version: 1.06
  * Generator template: C:\repo_github\fw_BasicAFE\02_Libraries\LIB_CSMON\05_Project\TEST_CSMON_C28xx\MotorParam\Template\
  *
  * Description: ...
