225b79e3142fe92f22f32f8db796020fc3d69ceb
Dimitar Lilov <devilerser@gmail.com>
2023-02-08 14:52:33 +0200

    CS1211 All versions work on SCIA

--- 05_Project/EXAMPLE_CSMON_C2806x/MotorParam/Generated/csmon_config.c
+++ 05_Project/EXAMPLE_CSMON_C2806x/MotorParam/Generated/csmon_config.c
@@ -4,9 +4,9 @@
  *
  * Created on 2019 08 14 12:51
  *
- * Generated on 02/06/2023 10:55:27 UTC
+ * Generated on 02/08/2023 11:24:14 UTC
  * Generator version: 1.12
- * Generator template: D:\repos\github_cs\fw_LIB_CSMON\05_Project\EXAMPLE_CSMON_C2806x\MotorParam\Template\
+ * Generator template: C:\repo_github\fw_BasicAFE\02_Libraries\LIB_CSMON\05_Project\EXAMPLE_CSMON_C2806x\MotorParam\Template\
  *
  * Description: ...
  * 
